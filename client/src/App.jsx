52730d38caabaefd3dfc98be5658e0190ffe2912
Ivelton Queiroz <iveltondequeiroz@gmail.com>
2023-07-22 22:49:42 -0300

    add pages and components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
+import Canvas from './canvas';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
+
+
+
 
 function App() {
   return (
-    <div>
-      <h1 className="head-text">AI 3D Costumizer</h1>
-    </div>
+    <main className='app transition-all-ease=in'>
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
   )
 }
 
