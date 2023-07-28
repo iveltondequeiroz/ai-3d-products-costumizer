0654dd85b6f0b7bf6eda3d22dcf3453a47058af9
Ivelton Queiroz <iveltondequeiroz@gmail.com>
2023-07-27 22:44:11 -0300

    fixed product shadows

--- client/src/canvas/CamRig.jsx
+++ client/src/canvas/CamRig.jsx
@@ -1,13 +1,14 @@
-import React, {useRef} from 'react';
+import React, { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
+
 import state from '../store';
 
-const CameraRig = ({ children }) => {
+const CamRig = ({ children }) => {
   const group = useRef();
   const snap = useSnapshot(state);
-  
+
   useFrame((state, delta) => {
     const isBreakpoint = window.innerWidth <= 1260;
     const isMobile = window.innerWidth <= 600;
@@ -33,8 +34,9 @@ const CameraRig = ({ children }) => {
       delta
     )
   })
-  
+
+
   return <group ref={group}>{children}</group>
 }
 
-export default CameraRig
+export default CamRig
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -2,21 +2,26 @@ import { Canvas }  from '@react-three/fiber';
 import { Environment, Center }  from '@react-three/drei';
 
 import Shirt from './Shirt';
-import CameraRig from './CameraRig';
 import Backdrop from './Backdrop';
+import CamRig from './CamRig';
 
 
 const CanvasModel = () => {
   return (
-    <Canvas>
+    <Canvas
+      shadows
+      camera={{position: [0,0,0], fov: 25}}
+      gl={{preserveDrawingBuffer: true}}
+      className="w-full max-w-full h-full transition-all ease-in"
+    >
       <ambientLight intensity={"0.5"} />
       <Environment preset="city" />
-      {<CameraRig>
-        {/* <Backdrop /> */}
+      {<CamRig>
+        <Backdrop />
         <Center>
           <Shirt />
         </Center>
-      </CameraRig>
+      </CamRig>
       }
     </Canvas>
   )
