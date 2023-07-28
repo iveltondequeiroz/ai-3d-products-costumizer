19af83d5269ff134d87efe28c4e90e055dbdc59d
Ivelton Queiroz <iveltondequeiroz@gmail.com>
2023-07-28 00:30:19 -0300

    add tabs functionalities

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -11,6 +11,73 @@ import { AIPicker, ColorPicker, FilePicker, Tab, CustomButton} from '../componen
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  }); 
+
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />
+      case "filepicker":
+        return <FilePicker
+          file={file}
+          setFile={setFile}
+          readFile={readFile}
+        />
+      case "aipicker":
+        return <AIPicker 
+          prompt={prompt}
+          setPrompt={setPrompt}
+          generatingImg={generatingImg}
+          handleSubmit={handleSubmit}
+        />
+      default:
+        return null;
+    }
+  }
+
+  const readFile = (type) => {
+    reader(file)
+      .then((result) => {
+        handleDecals(type, result);
+        setActiveEditorTab("");
+      })
+  }
+
+  const handleDecals = (type, result) => {
+    const decalType = DecalTypes[type];
+
+    state[decalType.stateProperty] = result;
+
+    if(!activeFilterTab[decalType.filterTab]) {
+      handleActiveFilterTab(decalType.filterTab)
+    }
+  }
+
+  const handleActiveFilterTab = (tabName) => {
+    switch (tabName) {
+      case "logoShirt":
+          state.isLogoTexture = !activeFilterTab[tabName];
+        break;
+      case "stylishShirt":
+          state.isFullTexture = !activeFilterTab[tabName];
+        break;
+      default:
+        state.isLogoTexture = true;
+        state.isFullTexture = false;
+        break;
+    }
+  }
+
+
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -26,9 +93,11 @@ const Customizer = () => {
                   <Tab
                   key={tab.name}
                   tab={tab}
-                  handleClick={()=> {}}
+                  handleClick={()=> setActiveEditorTab(tab.name)}
                 />
                 ))}
+
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,8 +5,8 @@ const state = proxy({
   color: '#EFBD48',
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: './threejs.png',
-  fullDecal: './threejs.png'
+  logoDecal: './logo-transp.png',
+  fullDecal: './logo-transp.png'
 });
 
 export default state;
